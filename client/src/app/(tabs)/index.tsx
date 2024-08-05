5a9080d446dcc78aea13bf4513a0c5358f43564b
synicalToo <weijielim@yahoo.com>
2024-08-05 12:36:21 +0800

    fixed nativewind vscode underline

--- client/src/app/(tabs)/index.tsx
+++ client/src/app/(tabs)/index.tsx
@@ -6,27 +6,16 @@ const TabOneScreen = () => {
   const { data, errorMsg, loading, postData } = usePost('/api/v1/detect');
 
   return (
-    <View style={styles.container}>
+    <View className='flex-1 justify-center items-center'>
       <Button title="Fetch Data" onPress={() => postData({ "area": "Ang Mo Kio" })} />
       {loading && <ActivityIndicator size="large" color="#0000ff" />}
       {errorMsg ? (
-        <Text style={styles.error}>{errorMsg}</Text>
+        <Text className='text-red-500 text-xl'>{errorMsg}</Text>
       ) : (
-        <Text>{data ? data.message : 'No data'}</Text>
+        <Text className='text-neutral-500 text-lg font-semibold'>{data ? data.message : 'No data'}</Text>
       )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  error: {
-    color: 'red',
-  },
-});
 
 export default TabOneScreen;
